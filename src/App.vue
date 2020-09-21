8fa6ca138ee8c7cfebf8ccef074ec3e3b9184908
Ross Domke <ross@domke.io>
2020-09-20 22:35:44 -0400

    Grid system complete

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,65 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
+    <block-grid :block="GameBoard"></block-grid>
+    <div class="game-pieces">
+      <block-grid
+        v-for="(piece, index) in PlayablePieces"
+        :block="piece"
+        :key="`playable-piece-${index}`"
+      ></block-grid>
+      <button @click="GeneratePieces">Gen</button>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
-import HelloWorld from "./components/HelloWorld.vue";
+import { Block, BlockCell, shortL } from "./Types";
+import BlockGrid from "./components/BlockGrid.vue";
+function randColor() {
+  return "#" + ((Math.random() * 0xffffff) << 0).toString(16);
+}
+function GridGenerator(
+  width: number,
+  height: number,
+  color: string = randColor()
+): Block {
+  const blockData: BlockCell[][] = [];
+  for (let h = 0; h < height; h += 1) {
+    blockData[h] = [];
+    for (let w = 0; w < width; w += 1) {
+      blockData[h][w] = { backgroundColor: color, filled: false };
+    }
+  }
+  const block = new Block(blockData);
+  return block;
+}
 
 @Component({
   components: {
-    HelloWorld
+    BlockGrid
   }
 })
-export default class App extends Vue {}
+export default class App extends Vue {
+  GameBoard: Block = GridGenerator(8, 8);
+  MaxPieces = 3;
+  PlayablePieces?: Block[] = [];
+  created() {
+    this.GeneratePieces();
+  }
+  GeneratePieces() {
+    this.PlayablePieces = [];
+    for (let p = 0; p < this.MaxPieces; p += 1) {
+      this.PlayablePieces[p] = new Block(shortL);
+      this.PlayablePieces[p].SetColor(randColor());
+      this.PlayablePieces[p].SetBackgroundColor();
+    }
+  }
+}
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "./theme.scss";
 </style>
---
+++ src/components/BlockGrid.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="block-grid" draggable>
+    <div
+      class="block-row"
+      v-for="(row, rIdx) in block.cells"
+      :key="`block-row-${rIdx}`"
+    >
+      <div
+        class="cell"
+        v-for="(cell, cIdx) in row"
+        :key="`block-cell-${rIdx}-${cIdx}`"
+        :style="{
+          'background-color': cell.filled ? cell.color : cell.backgroundColor
+        }"
+      ></div>
+    </div>
+  </div>
+</template>
+<script lang="ts">
+import { Component, Prop, Vue } from "vue-property-decorator";
+import { Block } from "../Types";
+
+@Component
+export default class BlockGrid extends Vue {
+  @Prop() private block!: Block;
+}
+</script>
+
+<style scoped lang="scss">
+.block-grid {
+  display: inline-block;
+}
+.block-row {
+  display: flex;
+  margin: 0px;
+  padding: 0px;
+}
+.cell {
+  display: inline-block;
+  width: 50px;
+  height: 50px;
+  margin: 2px;
+}
+</style>
